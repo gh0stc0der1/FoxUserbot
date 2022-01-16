1acb405b19d22d044a346341bd22f232f20f89f7
Terexdev <85295109+terexdev@users.noreply.github.com>
2022-01-16 20:25:02 +0300

    Update loadmod.py

--- plugins/loadmod.py
+++ plugins/loadmod.py
@@ -6,12 +6,15 @@ import os
 prefix = settings['prefix']
 
 @Client.on_message(filters.command('loadmod', prefixes=prefix) & filters.me)
-async def loadmod(client: Client, message: Message):
-    try:
-        link = message.command[1]
-        os.system(f'wget -P plugins/ {link}')
-        await message.edit("**The module has been loaded successfully.**")
-    except:
-        await message.edit("**An error has occurred**")
+async def load_module(client: Client, message: Message):
+	if not message.reply_to_message:
+		await message.edit("<b>Нету реплай!</b>")
+	else:
+		if not message.reply_to_message.document:
+			await message.edit("<b>Это не файл!</b>")
+		else:
+			await client.download_media(message.reply_to_message.document,file_name="plugins")
+			await message.edit("<b>Модуль загружен!</b>")
+			os.execvp("python3",["python3","main.py",],)
     
-module_list['Loadmod'] = f'{prefix}loadmod [link to the module]'
+module_list['Loadmod'] = f'{prefix}loadmod REPLY message'
